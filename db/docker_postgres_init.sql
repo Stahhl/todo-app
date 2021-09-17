1ad19cc58f40dcf097ef4459a0802f4434c0dbd1
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-09-17 22:07:41 +0200

    db reorder trigger-function

--- db/docker_postgres_init.sql
+++ db/docker_postgres_init.sql
@@ -1,5 +1,7 @@
+-- extension for uui:s
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+--create table
 create table todos
 (
     id         uuid DEFAULT uuid_generate_v4() primary key,
@@ -9,5 +11,35 @@ create table todos
     isComplete boolean not null default false
 );
 
+--seed table
 insert into todos (title,text) values
-('korv', 'laga korv');
+('lunch', 'ägg'),
+('middag', 'korv');
+
+--reorder trigger-function
+CREATE FUNCTION trg_reorder() RETURNS trigger AS
+$trg_reorder$
+DECLARE
+    t_row todos%rowtype;
+BEGIN
+    IF NEW.todoorder != OLD.todoorder THEN
+        FOR t_row in SELECT * FROM todos
+            LOOP
+                IF t_row.todoorder >= NEW.todoorder AND t_row.id != NEW.id THEN
+                    update todos
+                    set todoorder = todoorder + 1
+                    where id = t_row.id;
+                end if;
+            END LOOP;
+    END IF;
+    RETURN NULL;
+END;
+$trg_reorder$ LANGUAGE plpgsql;
+
+--reorder trigger
+CREATE TRIGGER trg_reorder
+    AFTER UPDATE
+        OF todoorder
+    ON todos
+    FOR EACH ROW
+EXECUTE FUNCTION trg_reorder();

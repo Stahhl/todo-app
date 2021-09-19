81e85ff8719944703c1d8f5a00bd82a20928c365
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-09-19 15:53:31 +0200

    express swagger

--- backend/expressjs/index.ts
+++ backend/expressjs/index.ts
@@ -1,8 +1,60 @@
-import express from 'express';
-// rest of the code remains same
+import express from "express";
+import { Pool } from "pg";
+import { Todo } from "./models/Todo";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+// db
+const connectionString = "postgresql://root:root@host.docker.internal:5432/root"
+
+const pool = new Pool({
+  connectionString
+});
+
+// express
+
 const app = express();
-const PORT = 5001;
-app.get('/', (req, res) => res.send('Express + TypeScriptt Server'));
+const PORT = 5011;
+
+// swagger
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "TodoApi",
+      description: "TodoApi - ExpressJS",
+      contact: {
+        name: "Amazing Developer"
+      },
+      servers: ["http://localhost:5011"]
+    }
+  },
+  apis: ["index.ts"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// routes
+/**
+ * @swagger
+ * /todos:
+ *  get:
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+app.get("/todos", async (req, res) => {
+  pool.query("SELECT * FROM todos", (err, result) => {
+    console.log(err, result);
+
+    res.json(result.rows as Todo[]);
+  });
+});
+
+// start
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
---
+++ backend/expressjs/models/Todo.ts
@@ -0,0 +1,7 @@
+export interface Todo {
+    id: string;
+    title: string;
+    text: string;
+    todoOrder: number;
+    isCompltete: boolean;
+}

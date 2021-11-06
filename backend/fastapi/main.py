9b768ee40bf6575c90f9e84a6c77b8e6c089a7b4
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-11-06 16:41:18 +0100

    fastapi scaffold actions

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -3,8 +3,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/todos")
 def index():
+    return {"message": "Hello World"}
+
+@app.get("/todos/{id}")
+def index(id: str):
+    return {"message": "Hello World"}
+
+@app.post("/todos")
+def index():
+    return {"message": "Hello World"}
+
+@app.delete("/todos/{id}")
+def index(id: str):
+    return {"message": "Hello World"}
+
+@app.put("/todos/{id}")
+def index(id: str):
+    return {"message": "Hello World"}
+
+@app.put("/todos/{id}/order")
+def index(id: str):
     return {"message": "Hello World"}
 
 if __name__ == "__main__":

4c8ca405b793cf48f1e52bfa2d1f49283aa906bf
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-11-06 17:49:41 +0100

    fastapi get actions

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,31 +1,56 @@
 import uvicorn
-from fastapi import FastAPI
+import databases
+from fastapi import FastAPI, Depends
+# from db import GetDb
 
+db = databases.Database(
+    "postgresql://root:root@host.docker.internal:5432/root")
 app = FastAPI()
 
+
+def GetDb():
+    return db
+
 @app.get("/todos")
-def index():
-    return {"message": "Hello World"}
+async def GetAll():
+    await db.connect()
+    sql = "SELECT * FROM todos"
+    result = await db.fetch_all(sql)
+    await db.disconnect()
+
+    return result
+
 
 @app.get("/todos/{id}")
-def index(id: str):
-    return {"message": "Hello World"}
+async def GetById(id: str):
+    await db.connect()
+    sql = "SELECT * FROM todos WHERE id = :id"
+    param = {"id": id}
+    result = await db.fetch_all(sql, param)
+    await db.disconnect()
+
+    return result
+
 
 @app.post("/todos")
 def index():
     return {"message": "Hello World"}
 
+
 @app.delete("/todos/{id}")
 def index(id: str):
     return {"message": "Hello World"}
+
 
 @app.put("/todos/{id}")
 def index(id: str):
     return {"message": "Hello World"}
 
+
 @app.put("/todos/{id}/order")
 def index(id: str):
     return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
